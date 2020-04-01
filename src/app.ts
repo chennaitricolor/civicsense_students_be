8b5b83f26f58a4344b565af1be67542a77e42252
Jegan Raj and Riyaz Ahamed <pair+jeganath1994+ssr0401@gmail.com>
2020-04-01 15:28:27 +0530

    Fix static path

--- src/app.ts
+++ src/app.ts
@@ -47,7 +47,7 @@ export class FastifyPluginRegister  {
       fastify.register(fastifyHelmet);
       fastify.register(fastifyCompress);
       fastify.register(require('fastify-static'), {
-        root: path.join(__dirname, 'static'),
+        root: path.join(__dirname, '..', 'static'),
       });
 
       // custom plugins
