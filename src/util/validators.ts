d8b74ffdced5eb408dc40ed70f0765c78e53db33
riyaz4s <ssr0401@gmail.com>
2020-05-20 04:23:04 +0530

    Adding validators for positive campaign

--- src/util/validators.ts
+++ src/util/validators.ts
@@ -1,6 +1,27 @@
+class RangeValidator {
+    private matchers: RegExpExecArray | null;
+    constructor(range: string) {
+        const rangeEx = /^(\(|\[)([\d]+),([\d]+)(\)|\])$/;
+        this.matchers = rangeEx.exec(range);
+    }
+
+    public validate(value: number) :boolean {
+        const matchers = this.matchers;
+        if (matchers) {
+            const lowNumber = parseInt(matchers[2], 10);
+            const highNumber = parseInt(matchers[3], 10);
+            const low = matchers[1] === '[' ? lowNumber : lowNumber - 1;
+            const high = matchers[4] === ']' ? highNumber : highNumber - 1;
+            return low <= value && value <= high;
+        }
+        return false;
+    }
+}
 export const validations = {
     isPositiveCaseValidators: {
-        Temperature : (temperature: number) =>  97 <= temperature && temperature <= 9
-
+        'Oxygen Level' : (ol: number) :boolean => new RangeValidator('[94, 98]').validate(ol),
+        'Pulse' : (pulse: number) :boolean=> new RangeValidator('[72, 80]').validate(pulse),
+        'Temperature' : (temperature: number) :boolean => new RangeValidator('[98, 99]').validate(temperature),
+        'Respiratory Rate' : (respiratorRate: number) :boolean => new RangeValidator('[16, 20]').validate(respiratorRate),
     }
 };
